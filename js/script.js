152cfa7b79f5f899f760f37224cbd2a40aa00eb6
Gianpaolo <pao.barga@gmail.com>
2021-01-18 16:47:02 +0100

    cambiato virgolette in apici

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Esercizio 1
 var studente = {
-    "nome": "Paolo",
-    "cognome": "Paoleschi",
-    "età": 19
+    'nome': 'Paolo',
+    'cognome': 'Paoleschi',
+    'età': 19
 }
 
 for(var key in studente) {
@@ -12,35 +12,35 @@ for(var key in studente) {
 // Esercizio 2
 var listaStudenti = [
     {
-        "nome": "studente1-nome",
-        "cognome": "studente1-cognome",
-        "età": 19
+        'nome': 'studente1-nome',
+        'cognome': 'studente1-cognome',
+        'età': 19
     },
     {
-        "nome": "studente2-nome",
-        "cognome": "studente2-cognome",
-        "età": 14
+        'nome': 'studente2-nome',
+        'cognome': 'studente2-cognome',
+        'età': 14
     },
     {
-        "nome": "studente3-nome",
-        "cognome": "studente3-cognome",
-        "età": 9
+        'nome': 'studente3-nome',
+        'cognome': 'studente3-cognome',
+        'età': 9
     },
     {
-        "nome": "studente4-nome",
-        "cognome": "studente4-cognome",
-        "età": 99
+        'nome': 'studente4-nome',
+        'cognome': 'studente4-cognome',
+        'età': 99
     },
     {
-        "nome": "studente5-nome",
-        "cognome": "studente5-cognome",
-        "età": 9999
+        'nome': 'studente5-nome',
+        'cognome': 'studente5-cognome',
+        'età': 9999
     }
 ];
 
 for(var i = 0; i < listaStudenti.length; i++)  {
     for(var prop in listaStudenti[i]) {
-        if(prop === "nome" || prop === "cognome") {
+        if(prop === 'nome' || prop === 'cognome') {
             console.log(`Studente n. ${i + 1}, ${prop}: ${listaStudenti[i][prop]}.`)
         }
     }
@@ -49,10 +49,10 @@ for(var i = 0; i < listaStudenti.length; i++)  {
 // Esercizio 3
 (function creaNuovoStudente(arr) {
     var nuovoStudente = {};
-    var listaProprietà = ["nome", "cognome", "età"];
+    var listaProprietà = ['nome', 'cognome', 'età'];
 
     for (var i = 0; i < listaProprietà.length; i++) {
-        listaProprietà[i] === "età" ?
+        listaProprietà[i] === 'età' ?
             nuovoStudente[listaProprietà[i]] = parseInt(prompt(`${listaProprietà[i]}:`)) :
             nuovoStudente[listaProprietà[i]] = prompt(`${listaProprietà[i]}:`);
     }
